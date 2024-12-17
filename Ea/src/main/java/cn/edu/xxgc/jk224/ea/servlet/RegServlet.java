e4b40a564883f903f732fb54b23f16c61de0fd92
Zhang Xianduo <157624656+buhuijiaojiao@users.noreply.github.com>
2024-12-17 18:12:52 +0800

    Add files via upload

---
+++ Ea/src/main/java/cn/edu/xxgc/jk224/ea/servlet/RegServlet.java
@@ -0,0 +1,32 @@
+package cn.edu.xxgc.jk224.ea.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/reg")
+public class RegServlet extends HttpServlet {
+
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter pw=resp.getWriter();
+        pw.println("奥里给");
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+
+    public void destroy() {
+        super.destroy();
+    }
+
+
+}
