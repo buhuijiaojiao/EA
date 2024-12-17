9d93fc4d65e7283ef89fbd6ed04c2c1cfed805ca
git_user1 <3317038611@qq.com>
2024-12-17 20:58:27 +0800

    Changes

--- Ea/src/main/java/cn/edu/xxgc/jk224/ea/servlet/LoginServlet.java
+++ Ea/src/main/java/cn/edu/xxgc/jk224/ea/servlet/LoginServlet.java
@@ -9,22 +9,5 @@ import java.io.PrintWriter;
 
 public class LoginServlet extends HttpServlet {
 
-    public void init() throws ServletException {
-        super.init();
-    }
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html; charset=UTF-8");
-        PrintWriter pw=resp.getWriter();
-        pw.println("奥里给");
-        pw.println("damn");
-    }
-
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req,resp);
-    }
-
-    public void destroy() {
-        super.destroy();
-    }
 }
