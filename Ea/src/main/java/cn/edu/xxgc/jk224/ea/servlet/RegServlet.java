765e51ef68ebd62cf2675f2f2d43b6d30b1ccb18
buhuijiaojiao <zxd13781369670@gmail.com>
2024-12-18 14:34:58 +0800

    一些参数名修正

--- Ea/src/main/java/cn/edu/xxgc/jk224/ea/servlet/RegServlet.java
+++ Ea/src/main/java/cn/edu/xxgc/jk224/ea/servlet/RegServlet.java
@@ -16,6 +16,7 @@ public class RegServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse  response) throws ServletException, IOException {
          response.setContentType("text/html; charset=UTF-8");
 
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse  response) throws ServletException, IOException {
