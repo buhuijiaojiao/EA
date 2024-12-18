cee50cb54afa2aa06c81bd7d5328a7b47e0a13a7
buhuijiaojiao <zxd13781369670@gmail.com>
2024-12-18 22:38:15 +0800

    展示，添加学生信息servlet

---
+++ Ea/src/main/java/cn/edu/xxgc/jk224/ea/servlet/AddStudentServlet.java
@@ -0,0 +1,64 @@
+package cn.edu.xxgc.jk224.ea.servlet;
+
+
+import cn.edu.xxgc.jk224.ea.entity.Student;
+import cn.edu.xxgc.jk224.ea.util.DBUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet("/add-student")
+public class AddStudentServlet extends HttpServlet {
+
+    public AddStudentServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, UnsupportedEncodingException {
+        request.setCharacterEncoding("utf-8");
+        //获取表单输入的学生信息
+        String id = request.getParameter("Sid");
+        String name = request.getParameter("studentName");
+        String sex = request.getParameter("studentSex");
+        String college = request.getParameter("college");
+        String major = request.getParameter("studentMajor");
+        Student student = new Student(id, name, sex, college, major);
+
+
+        int rows = 0;
+        try {
+            Connection conn = DBUtil.getConnection();
+
+            String sql = "insert into `student` (`Sid`, `sname`, `ssex`, `scollege`, `smajor`) VALUES (?, ?, ?, ?,?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, student.getId());
+            pstmt.setString(2, student.getName());
+            pstmt.setString(3, student.getSex());
+            pstmt.setString(3, student.getCollege());
+            pstmt.setString(3, student.getMajor());
+            rows = pstmt.executeUpdate();
+            DBUtil.destory(conn, pstmt, null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (rows > 0) {
+            response.sendRedirect(request.getContextPath() + "/index.jsp");
+        } else {
+            //添加失败
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
---
+++ Ea/src/main/java/cn/edu/xxgc/jk224/ea/servlet/ListStudentServlet.java
@@ -0,0 +1,61 @@
+package cn.edu.xxgc.jk224.ea.servlet;
+
+import cn.edu.xxgc.jk224.ea.entity.Student;
+import cn.edu.xxgc.jk224.ea.util.*;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.HashSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/set-student")
+public class ListStudentServlet extends HttpServlet {
+    public ListStudentServlet() {
+        super();
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HashSet<Student> students = null;
+        try {
+            Connection conn = DBUtil.getConnection();
+
+            String sql = "select * from student";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            //查询操作较多且无需考虑数据
+            students = new HashSet<>();
+            while (rs.next()) {
+                Student student = new Student();
+                student.setId(rs.getString("Sid"));
+                student.setName(rs.getString("sname"));
+                student.setSex(rs.getString("ssex"));
+                student.setCollege(rs.getString("scollege"));
+                student.setMajor(rs.getString("smajor"));
+                students.add(student);
+            }
+
+            DBUtil.destory(conn, pstmt, rs);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        request.setAttribute("studentInfo", students);
+        //转发请求到首页
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
